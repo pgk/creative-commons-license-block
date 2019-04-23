ee378a4bac9b998e0f482c9da4efba418b4b54ec
Glitch (hello-express) <none>
2019-04-23 13:46:07 +0000

    👸✈️ Checkpoint

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -25843,7 +25843,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40317" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43475" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
