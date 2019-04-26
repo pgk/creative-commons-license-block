73e1bb89381025392f63236e57bb2a45decca3ac
Glitch (gutenberg-block-kit) <none>
2019-04-26 19:42:30 +0000

    💁🌸 Checkpoint ./server.js:260226/16

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require( 'express' )();
 const Parcel = require( 'parcel-bundler' );
 const parcel = new Parcel( 'src/index.html', { contentHash: false } );
 
-const plugin = require( './lib/plugin' );
+const pluginMaker = require( './lib/plugin-maker' );
 const apiMock = require( './lib/wp-api-mock' );
 
 // http://expressjs.com/en/starter/basic-routing.html
@@ -16,7 +16,7 @@ app.get( '/wp/v2/types/wp_block', apiMock.typesWPBlock );
 app.get( '/wp/v2/blocks', apiMock.blocks );
 
 // Serve up the block as a WP Plugin
-app.get( '/plugin.zip', plugin( parcel ) );
+app.get( '/plugin.zip', pluginMaker( parcel ) );
 
 // Send remaining requests to parcel
 app.use( parcel.middleware() );
