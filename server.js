7bdd54bba0d064e77d8c6f659cc15dd0e75218ce
Glitch (gutenberg-block-kit) <none>
2019-04-26 18:37:30 +0000

    🚲🚌 Checkpoint ./lib/wp-api-mock.js:260226/4013 ./server.js:260226/1538

--- server.js
+++ server.js
@@ -7,36 +7,14 @@ const Parcel = require( 'parcel-bundler' );
 const parcel = new Parcel( 'src/index.html', { contentHash: false } );
 
 const plugin = require( './lib/plugin' );
+const apiMock = require( './lib/wp-api-mock' );
 
 // http://expressjs.com/en/starter/basic-routing.html
 
 // Mock out some API responses that Gutenberg expects:
-app.get( '/wp/v2/types/wp_block', function( request, response) {
-    response.setHeader( 'Content-Type', 'application/json' );
-    response.end( JSON.stringify( {
-      "capabilities": { },
-      "description": "",
-      "hierarchical": false,
-      "labels": {},
-      "name": "Blocks",
-      "slug": "wp_block",
-      "taxonomies": [],
-      "rest_base": "blocks",
-      "supports": {},
-      "viewable": false,
-      "_links": {}
-  } ) );
-} );
+app.use( '/wp/v2/:path', apiMock );
 
-app.get( '/wp/v2/blocks', function( request, response) {
-    response.setHeader( 'Content-Type', 'application/json' );
-    response.end( JSON.stringify( {
-    content: '<!-- cgb/block-my-block -->↵<div class="wp-block-cgb-block-my-block"><p>— Hello from the frontend.</p><p>CGB BLOCK: <code>my-block</code> is a new Gutenberg block.</p><p>It was created via <code><a href="https://github.com/ahmadawais/create-guten-block">create-guten-block</a></code>.</p></div><!-- /cgb/block-my-block -->',
-    id: 42,
-    title: "Guten Tag You're It",
-  } ) );
-} );
-
+// Serve up a plugin ZIP file
 app.use( '/plugin.zip', plugin.middleware( parcel ) );
 
 // Send remaining requests to parcel
