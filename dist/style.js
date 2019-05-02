7ab1f29e7c9596f575ab423071c2b9e29c2f71c0
Glitch (gutenberg-block-kit) <none>
2019-05-02 17:37:38 +0000

    🏏🙌 Checkpoint ./server.js:260226/38 ./src/index.html:260226/48

--- dist/style.js
+++ dist/style.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36324" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43675" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -393,4 +393,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../rbd/pnpm-volume/d2032613-1317-456e-be8e-bc0af5fd945c/node_modules/.registry.npmjs.org/parcel-bundler/1.12.3/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.97fcb138.js.map
+//# sourceMappingURL=/style.js.map
