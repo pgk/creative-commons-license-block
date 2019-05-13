dd74efbd778d9064795f7ab82809f82eb14ad9a1
Glitch (gutenberg-block-kit) <none>
2019-05-13 20:56:08 +0000

    🦄🚁 Checkpoint ./.glitch-assets:260226/2970 ./src/index.html:260226/934 ./src/index.scss:260226/3469

--- .dist/src.e31bb0bc.js
+++ .dist/src.e31bb0bc.js
@@ -26129,7 +26129,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41112" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40069" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
